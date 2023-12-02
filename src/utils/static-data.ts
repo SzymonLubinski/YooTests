22db9f1ceb27288843ba55e7bec413213d5a255e
Szymon Lubinski <lubinskis90@gmail.com>
2023-12-02 12:36:29 +0100

    new static data

--- src/utils/static-data.ts
+++ src/utils/static-data.ts
@@ -7,93 +7,93 @@ import {Product} from "../types/db";
 export const staticData: Product[] = [
     {
         id: '1',
-        name: 'nike',
+        name: 'shoes #1',
         images: [Picture4, Picture3, Picture1, Picture2, Picture1, Picture4],
         price: 23.99,
-        description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum'
+        description: 'Lorem ipsum dolor sit amet. Et suscipit enim ut vero sint At internos debitis ut earum iure 33 ullam incidunt. Ab voluptatibus earum est exercitationem ipsa aut nihil placeat in sunt amet sit quisquam fugiat et accusantium eaque.'
     },
     {
         id: '2',
-        name: 'F22',
+        name: 'shoes #2 - Lorem ipsum dolor sit amet ',
         images: [Picture3, Picture1, Picture2],
         price: 100.99,
-        description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum'
+        description: 'Et suscipit enim ut vero sint At internos debitis ut earum iure 33 ullam incidunt. '
     },
     {
         id: '3',
-        name: 'shoes #3',
+        name: 'shoes #3 - Et suscipit enim ut vero sint At internos debitis',
         images: [Picture1, Picture2, Picture3],
         price: 120.99,
-        description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum'
+        description: 'Lorem ipsum dolor sit amet. '
     },
     {
         id: '4',
-        name: 'new brand shoes to good using #4',
+        name: 'shoes #4 - Lorem ipsum dolor sit amet',
         images: [Picture1, Picture2],
         price: 15.99,
-        description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum'
+        description: 'Lorem ipsum dolor sit amet. '
     },
     {
         id: '5',
-        name: 'Jack',
+        name: 'shoes #5',
         images: [Picture1, Picture3, Picture2],
         price: 20.99,
-        description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum'
+        description: 'Et sunt mollitia et sunt expedita aut quas distinctio quo voluptates deserunt cum aperiam illum. '
     },
     {
         id: '6',
-        name: 'shoes #7',
+        name: 'shoes #6',
         images: [Picture1, Picture2],
         price: 1220.99,
-        description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum'
+        description: 'Et suscipit enim ut vero sint At internos debitis ut earum iure 33 ullam incidunt. '
     },
     {
         id: '7',
-        name: 'very good new brand shoes to good using #8',
+        name: 'shoes #7',
         images: [Picture1, Picture2],
         price: 125.99,
-        description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum'
+        description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsumLorem ipsum dolor sit amet. Et suscipit enim ut vero sint At internos debitis ut earum iure 33 ullam incidunt. Ab voluptatibus earum est exercitationem ipsa aut nihil placeat in sunt amet sit quisquam fugiat et accusantium eaque.'
     },
     {
         id: '8',
-        name: 'shoes #8',
+        name: 'shoes #8 - Et suscipit enim ut vero sint At internos debitis',
         images: [Picture1, Picture2],
         price: 100.99,
-        description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum'
+        description: 'Ab ipsa quod non aperiam voluptas in commodi repellat id distinctio obcaecati et consequatur rerum a doloribus incidunt aut iusto placeat. Qui earum voluptates aut quia veniam ad illum sint qui omnis incidunt est voluptas pariatur aut quia magnam. Non quod quam in nemo doloribus in distinctio praesentium qui dolor odio vel necessitatibus ullam est recusandae impedit et nihil commodi. Et nihil quam ut autem autem et saepe consequatur aut dolore consequatur!'
     },
     {
         id: '9',
-        name: 'puma',
+        name: 'shoes #9 - Lorem ipsum',
         images: [Picture1, Picture2],
         price: 120.99,
-        description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum'
+        description: 'Lorem ipsum dolor sit amet. '
     },
     {
         id: '10',
-        name: 'cool!',
+        name: 'shoes #10!',
         images: [Picture1, Picture2],
         price: 15.99,
-        description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum'
+        description: 'Ab ipsa quod non aperiam voluptas in commodi repellat id distinctio obcaecati et consequatur rerum a doloribus incidunt aut iusto placeat. Qui earum voluptates aut quia veniam ad illum sint qui omnis incidunt est voluptas pariatur aut quia magnam. Non quod quam in nemo doloribus in distinctio praesentium qui dolor odio vel necessitatibus ullam est recusandae impedit et nihil commodi. Et nihil quam ut autem autem et saepe consequatur aut dolore consequatur!'
     },
     {
         id: '11',
         name: 'shoes #11',
         images: [Picture1, Picture2],
         price: 20.99,
-        description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum'
+        description: 'Et sunt mollitia et sunt expedita aut quas distinctio quo voluptates deserunt cum aperiam illum. '
     },
     {
         id: '12',
-        name: 'radeon',
+        name: 'shoes #12 - Et suscipit enim ut vero sint At internos debitis',
         images: [Picture1, Picture2],
         price: 1220.99,
-        description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum'
+        description: 'Et sunt mollitia et sunt expedita aut quas distinctio quo voluptates deserunt cum aperiam illum. '
     },
     {
         id: '13',
-        name: 'very good new brand shoes to good using #13',
+        name: 'shoes #13 - Lorem ipsum dolor sit amet',
         images: [Picture1, Picture2],
         price: 125.99,
-        description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum'
+        description: 'Ab ipsa quod non aperiam voluptas in commodi repellat id distinctio obcaecati et consequatur rerum a doloribus incidunt aut iusto placeat. Qui earum voluptates aut quia veniam ad illum sint qui omnis incidunt est voluptas pariatur aut quia magnam. Non quod quam in nemo doloribus in distinctio praesentium qui dolor odio vel necessitatibus ullam est recusandae impedit et nihil commodi. Et nihil quam ut autem autem et saepe consequatur aut dolore consequatur!'
     }
 ]
